dea736cacafe5b096d169c80241fe205cc9efc4d
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-26 15:34:07 -0300

    Update Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp

--- Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp
+++ Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp
@@ -7,26 +7,29 @@ int main(){
 //•Se o índice de a for par, insira em b o respectivo valor de a multiplicado por 5;
 //•Se o índice do vetor a for ímpar, insira em b o respectivo valor a somado com 5;
 //Ao final, o programa deve exibir o conteúdo dos vetores a e b.
-	setlocale(LC_ALL,"Portuguese");
-	int a[5], b[5], i;
-	for(i = 0; i < 5; i++){
-		printf("Informe o %iº número: ", i+1);
-		scanf("%i", &a[i]);
-	}
-	for(i = 0; i < 5; i++){
-		if(a[i] == 0) {
-           	 b[i] = a[i];
-           	 printf("O valor de a[%i] = %i e o de b[%i] = %i\n\n", i+1, a[i], i+1, b[i]);
-        } 
-        	else if(a[i] % 2 == 0) {
-            	b[i] = a[i] * 5;
-            	printf("O valor de a[%i] = %i e o de b[%i] = %i\n\n", i+1, a[i], i+1, b[i]);
-       		 }
-        	else if(a[i] % 2 != 0) {
-           		b[i] = a[i] + 5;
-           	 	printf("O valor de a[%i] = %i e o de b[%i] = %i\n\n", i+1, a[i], i+1, b[i]);
-       		 }
+    setlocale(LC_ALL, "Portuguese");
+    float a[5], b[5];
+    int i;
+    
+    for(i = 0; i < 5; i++){
+        printf("Informe o %.1dº número: ", i+1);
+        scanf("%f", &a[i]);
     }
-    return 0;    
+    
+    for(i = 0; i < 5; i++){
+        if(a[i] == 0) {
+            b[i] = a[i];
+            printf("\nO valor de a[%.1d] = %.1f e o de b[%.1d] = %.1f\n\n", i+1, a[i], i+1, b[i]);
+        }
+        else if(a[i] % 2 == 0){
+            b[i] = a[i] * 5;
+            printf("\nO valor de a[%.1d] = %.1f e o de b[%.1d] = %.1f\n\n", i+1, a[i], i+1, b[i]);
+        }
+        else if(a[i] % 2 != 0){
+            b[i] = a[i] + 5;
+            printf("\nO valor de a[%.1d] = %.1f e o de b[%.1d] = %.1f\n\n", i+1, a[i], i+1, b[i]);
+        }
+    }
+    
+    return 0;
 }
-
