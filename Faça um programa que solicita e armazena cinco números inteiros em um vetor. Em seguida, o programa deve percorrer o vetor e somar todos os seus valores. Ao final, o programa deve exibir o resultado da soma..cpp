361c6c9ccdca2da94bb726a0ca812e59e6064761
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-26 10:05:44 -0300

    Update and rename Lista de C 5 Questão 3.cpp to Faça um programa que solicita e armazena cinco números inteiros em um vetor. Em seguida, o programa deve percorrer o vetor e somar todos os seus valores. Ao final, o programa deve exibir o resultado da soma..cpp

--- Faça um programa que solicita e armazena cinco números inteiros em um vetor. Em seguida, o programa deve percorrer o vetor e somar todos os seus valores. Ao final, o programa deve exibir o resultado da soma..cpp
+++ Faça um programa que solicita e armazena cinco números inteiros em um vetor. Em seguida, o programa deve percorrer o vetor e somar todos os seus valores. Ao final, o programa deve exibir o resultado da soma..cpp
@@ -5,10 +5,10 @@ int main(){
 	setlocale(LC_ALL,"Portuguese");
 int n[5], i, s = 0;
 	for(i = 0; i < 5; i++){
-	printf("Informe o %iº número: ", i+1);
+	printf("Informe o %iÂº nÃºmero: ", i+1);
 	scanf("%i", &n[i]);
 		s+=n[i];
 }
-	printf("\nA soma dos números informados é: %i ", s);
+	printf("\nA soma dos nÃºmeros informados Ã©: %i ", s);
 return(0);
 }
