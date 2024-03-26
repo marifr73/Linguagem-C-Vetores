5e6e78d3a020a7017659897ed19ca56e0a024d96
Maria Francisca <141454612+marifr73@users.noreply.github.com>
2024-03-26 10:05:09 -0300

    Update and rename Lista de C 5 Questão 1.cpp to Escreva um programa que solicita e armazena oito valores inteiros em um vetor, denominado de A, e exibe os valores armazenados, porém na ordem inversa..cpp

--- Escreva um programa que solicita e armazena oito valores inteiros em um vetor, denominado de A, e exibe os valores armazenados, porém na ordem inversa..cpp
+++ Escreva um programa que solicita e armazena oito valores inteiros em um vetor, denominado de A, e exibe os valores armazenados, porém na ordem inversa..cpp
@@ -5,7 +5,7 @@ int main(){
 	setlocale(LC_ALL,"Portuguese");
 	int A[8], i;
 	for(i = 0; i < 8; i++){
-		printf("Informe o %iº números: ", i+1);
+		printf("Informe o %iÂº nÃºmeros: ", i+1);
 		scanf("%i", &A[i]);
 	}
 	for(i = 7; i >= 0; i--){
