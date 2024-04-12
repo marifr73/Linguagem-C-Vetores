2e4fc1c593a09632a1f60e2d4f4e17b745bc2412
Pedro Carlos <58538765+PedroCarlos30@users.noreply.github.com>
2024-04-12 17:05:39 -0300

    Update Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp

--- Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp
+++ Elabore um programa que solicita e armazena cinco números reais em um vetor, denominado de a, sendo que esses números serão utilizados para preencher outro vetor. Sendo assim, declare outro vetor, denominando de b, do mesmo tipo e tamanho de a e, em seguida, o preencha de acordo com as seguintes condições:.cpp
@@ -7,6 +7,7 @@ int main(){
 //•Se o índice de a for par, insira em b o respectivo valor de a multiplicado por 5;
 //•Se o índice do vetor a for ímpar, insira em b o respectivo valor a somado com 5;
 //Ao final, o programa deve exibir o conteúdo dos vetores a e b.
+    
     setlocale(LC_ALL, "Portuguese");
     float a[5], b[5];
     int i;
